eba11a05ffc3f5aad87aa8d61db0ff667ecda263
Eric Cunha <ericscunha@hotmail.com>
2020-01-04 18:26:49 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -25,8 +25,21 @@ echo json_encode($lista); */
 $search = Usuario::search("jo");
 echo json_encode($search); */
 
-/* Carrega um usuário usando o login e a senha */
+/* Carrega um usuário usando o login e a senha 
 $usuario = new Usuario();
 $usuario->login("root", "#t=$");
+echo $usuario;*/
+
+/* Insert de usuário novo */
+//$aluno = new Usuario();
+/* obs.: Pode criar um metódo cosntrutor (__construct($login = "", $password = "")) na classe passando os campos como parametros não obrigatórios, e chamando setDeslogin e setDessenha no corpo o
+que eliminaria a necessidade de chamar abaixo 
+$aluno->setDeslogin("aluno");
+$aluno->setDessenha("@lun0");
+$aluno->insert();
+echo $aluno;*/
+$usuario = new Usuario();
+$usuario->loadbyId(6);
+$usuario->update("professor","!@#$&");
 echo $usuario;
 ?>
